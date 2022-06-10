483df9fdcba0b67223cfcc81b81c8e352866d261
somasundaram1997 <somasundarams23@gmail.com>
2022-06-10 12:13:24 +0530

    project structure

---
+++ src/test/java/org/test/git/SampleGit.java
@@ -0,0 +1,26 @@
+package org.test.git;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleGit {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+		WebElement txtUsername = driver.findElement(By.id("email"));
+		txtUsername.sendKeys("sunadarm");
+		
+		WebElement txtPassword = driver.findElement(By.id("pass"));
+		txtPassword.sendKeys("989898");
+		
+		WebElement btnLogin = driver.findElement(By.name("login"));
+		btnLogin.click();
+		
+	}
+}
